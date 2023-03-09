35acbd40edfaee469e98754aebe47727936d1b0f
stefan-b10 <stefan_budai88@yahoo.com>
2023-03-09 20:23:19 +0200

    modified:   angular.json modified:   package-lock.json modified:   package.json modified:   src/app/app.component.html modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { ethers } from 'ethers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
+  // title = 'app';
+  blockNumber: number | string | undefined;
+  provider: ethers.providers.BaseProvider;
+  transactions: string[] | undefined;
+
+  constructor() {
+    this.provider = ethers.getDefaultProvider('goerli');
+  }
+
+  syncBlock() {
+    this.blockNumber = 'loading...';
+    this.provider.getBlock('latest').then((block) => {
+      this.blockNumber = block.number;
+      this.transactions = block.transactions;
+    });
+  }
+
+  clearBlock() {
+    this.blockNumber = 0;
+    this.transactions = [];
+  }
 }
